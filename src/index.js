7e8ba0651501d014224b5413f22c7bd43a117a4b
chaimae <56132945+s-shemmee@users.noreply.github.com>
2023-03-18 17:58:53 +0000

    Add Activities Caroussel

--- src/index.js
+++ src/index.js
@@ -239,3 +239,41 @@ document
   .addEventListener("click", toggleTempUnit);
 
 
+// Activities Carousel  
+
+const carousel = document.querySelector('.activity-carousel');
+const prevBtn = document.querySelector('.prev');
+const nextBtn = document.querySelector('.next');
+const activities = document.querySelectorAll('.activity');
+
+let currentSlide = 0;
+const numSlides = activities.length;
+
+prevBtn.addEventListener('click', () => {
+  currentSlide = (currentSlide - 1 + numSlides) % numSlides;
+  displaySlide();
+});
+
+nextBtn.addEventListener('click', () => {
+  currentSlide = (currentSlide + 1) % numSlides;
+  displaySlide();
+});
+
+function displaySlide() {
+  activities.forEach((activity, index) => {
+    if (index === currentSlide) {
+      activity.style.display = 'flex';
+    } else {
+      activity.style.display = 'none';
+    }
+  });
+}
+
+displaySlide();
+
+setInterval(() => {
+  nextBtn.click();
+}, 5000);
+
+
+
